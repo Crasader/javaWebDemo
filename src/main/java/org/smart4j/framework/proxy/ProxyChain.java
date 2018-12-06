d2758362922fba97c4c8d1734729dc399fdab58b
Administrator <Administrator@192.168.1.101>
2018-12-06 21:51:06 +0800

    aop

---
+++ src/main/java/org/smart4j/framework/proxy/ProxyChain.java
@@ -0,0 +1,65 @@
+package org.smart4j.framework.proxy;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.cglib.proxy.MethodProxy;
+
+public class ProxyChain {
+
+	private final Class<?> targetClass;
+	
+	private final Object targetObjet;
+	
+	private final Method targetMethod;
+	
+	private final MethodProxy methodProxy;
+	
+	private final Object[] methodParams;
+	
+	private List<Proxy> proxyList = new ArrayList<Proxy>(); 
+	
+	private int proxyIndex=0;
+
+
+	public ProxyChain(Class<?> targetClass, Object targetObjet, Method targetMethod, MethodProxy methodProxy,
+			Object[] methodParams, List<Proxy> proxyList) {
+		super();
+		this.targetClass = targetClass;
+		this.targetObjet = targetObjet;
+		this.targetMethod = targetMethod;
+		this.methodProxy = methodProxy;
+		this.methodParams = methodParams;
+		this.proxyList = proxyList;
+	}
+
+	public Object getTargetObjet() {
+		return targetObjet;
+	}
+
+	public Method getTargetMethod() {
+		return targetMethod;
+	}
+
+
+
+
+	public MethodProxy getMethodProxy() {
+		return methodProxy;
+	}
+
+
+
+
+	public Class<?> getTargetClass() {
+		return targetClass;
+	}
+	
+	
+	public Object doProxyChain() {
+		return null;
+		
+	}
+	
+}
